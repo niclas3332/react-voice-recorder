f35c797d1ae0907920f5a81e960869ab5c203b5e
niclaspapst <niclas.papst@gmail.com>
2022-06-08 02:20:22 +0200

    Updates

---
+++ precache-manifest.cdc268fa8788ddc594cc2506d9a3eeb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13173e95ecd45eab494acecde5cfa323",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e800b63abfa9afe5143b",
+    "url": "./static/css/main.1e7272e3.chunk.css"
+  },
+  {
+    "revision": "3fee9baa53a5468a8073",
+    "url": "./static/js/2.e5d914bb.chunk.js"
+  },
+  {
+    "revision": "f7adbd266a09abea79a62d44bf1152c3",
+    "url": "./static/js/2.e5d914bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e800b63abfa9afe5143b",
+    "url": "./static/js/main.ef9adf78.chunk.js"
+  },
+  {
+    "revision": "3d155985de06c3e8b716",
+    "url": "./static/js/runtime-main.29bb28ba.js"
+  }
+]);
